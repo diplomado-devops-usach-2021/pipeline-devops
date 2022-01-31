484edfb6888a0405a9bbce60c3bf4bda8451e24a
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2022-01-31 20:21:38 -0300

    nexo entre vars y src

--- src/helpers/Git.groovy
+++ src/helpers/Git.groovy
@@ -1,11 +1,16 @@
 package helpers
 
-def merge(){
-	println 'Este método realiza un merge'
+def merge(String ramaOrigen, String ramaDestino){
+	println "Este método realiza un merge ${ramaOrigen} y ${ramaDestino}"
 }
 
-def tag(){
-	println 'Este método realiza un tag'
+def tag(String ramaOrigen){
+	println "Este método realiza un tag en master de ${ramaOrigen}"
+
+	/*
+		- validar que la rama release cumpla con una nomenclatura definida: release-v(major)-(minor)-(patch)
+		- obtener el valor desde la "v" en adelante, ejemplo: release-v1-0-0 ... obtener sólo 1-0-0
+	*/
 }
 
 return this;
