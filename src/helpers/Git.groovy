36a8d51102591f1a35cd02a4b955019ab27941d6
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2022-01-31 21:23:09 -0300

    implementacion metodo tag

--- src/helpers/Git.groovy
+++ src/helpers/Git.groovy
@@ -12,15 +12,16 @@ def merge(String ramaOrigen, String ramaDestino){
 	"""
 }
 
-def tag(String ramaOrigen){
+def tag(String ramaOrigen, String ramaDestino){
 	println "Este método realiza un tag en master de ${ramaOrigen}"
 
 	if (ramaOrigen.contains('release-v')){
-		//cortar la variable y dejar solamente v1-0-0
-
-		def tagValue = ramaOrigen.split('release-v') // ['','v1-0-0']
-
-		tagValue.each { println it }
+		checkout(ramaDestino)
+		def tagValue = ramaOrigen.split('release-')[1]
+		sh """
+			git tag ${tagValue}
+			git push origin ${tagValue}
+		"""
 
 	} else {
 		error "La rama ${ramaOrigen} no cumple con nomenclatura definida para rama release (release-v(major)-(minor)-(patch))."
