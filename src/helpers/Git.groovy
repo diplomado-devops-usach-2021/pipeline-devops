b02dc4eb5957869b788e9ba6ab1baf5ebe634cb4
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2022-01-31 21:14:47 -0300

    implementacion metodo tag

--- src/helpers/Git.groovy
+++ src/helpers/Git.groovy
@@ -15,10 +15,16 @@ def merge(String ramaOrigen, String ramaDestino){
 def tag(String ramaOrigen){
 	println "Este método realiza un tag en master de ${ramaOrigen}"
 
-	/*
-		- validar que la rama release cumpla con una nomenclatura definida: release-v(major)-(minor)-(patch)
-		- obtener el valor desde la "v" en adelante, ejemplo: release-v1-0-0 ... obtener sólo 1-0-0
-	*/
+	if (ramaOrigen.contains('release-v')){
+		//cortar la variable y dejar solamente v1-0-0
+
+		def tagValue = ramaOrigen.split('release-v') // ['','v1-0-0']
+
+		tagValue.each { println it }
+
+	} else {
+		error "La rama ${ramaOrigen} no cumple con nomenclatura definida para rama release (release-v(major)-(minor)-(patch))."
+	}
 }
 
 def checkout(String rama){
--- vars/maven.groovy
+++ vars/maven.groovy
@@ -10,37 +10,42 @@ def call(String pipelineType){
 
   figlet 'Maven'
 
-  if (pipelineType == 'CI'){
-    figlet 'Integracion Continua'
+  def git = helpers.Git()
 
-    stage('Compile') {
-      STAGE = env.STAGE_NAME
-      sh './mvnw clean compile -e'
-    }
+  git.tag(env.GIT_LOCAL_BRANCH)
 
-    stage('Test') {
-      STAGE = env.STAGE_NAME
-      sh './mvnw clean test -e'
-    }
 
-    stage('Jar') {
-      STAGE = env.STAGE_NAME
-      sh './mvnw clean package -e'
-    }
-  } else {
-    figlet 'Delivery Continuo'
+  // if (pipelineType == 'CI'){
+  //   figlet 'Integracion Continua'
+
+  //   stage('Compile') {
+  //     STAGE = env.STAGE_NAME
+  //     sh './mvnw clean compile -e'
+  //   }
+
+  //   stage('Test') {
+  //     STAGE = env.STAGE_NAME
+  //     sh './mvnw clean test -e'
+  //   }
+
+  //   stage('Jar') {
+  //     STAGE = env.STAGE_NAME
+  //     sh './mvnw clean package -e'
+  //   }
+  // } else {
+  //   figlet 'Delivery Continuo'
     
-    stage('Run') {
-      STAGE = env.STAGE_NAME
-      sh 'nohup bash mvnw spring-boot:run &'
-      sleep 20
-    }
+  //   stage('Run') {
+  //     STAGE = env.STAGE_NAME
+  //     sh 'nohup bash mvnw spring-boot:run &'
+  //     sleep 20
+  //   }
     
-    stage('TestApp') {
-      STAGE = env.STAGE_NAME
-      sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-    }
-  }
+  //   stage('TestApp') {
+  //     STAGE = env.STAGE_NAME
+  //     sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+  //   }
+  // }
 
 }
 
